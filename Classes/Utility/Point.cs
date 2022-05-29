b9aa49a9d3441850761f0001b0b6fd713d43b6a9
Lir0sis <suparboivalik@gmail.com>
2022-05-29 13:25:09 +0300

    Placeholders

---
+++ Classes/Utility/Point.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Classes.Utility
+{
+    class Point
+    {
+        private float x;
+        private float y;
+        private float z;
+
+        public Point(float x, float y, float z)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+
+
+
+    }
+}
--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Classes;
 
 namespace Lab1
 {
